869c98ece138b4cfae6a338b43f6a42282c5493c
Kam <26062599+Kamil3001@users.noreply.github.com>
2025-12-06 01:26:49 +0000

    Astra tools

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "711353141"
+    serviceWorkerVersion: "1914896156"
   }
 });
